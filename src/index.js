d32917789497f7878373d343d483b153abf3e2dd
kotomi1338 <kotomin1216@gmail.com>
2019-09-08 22:05:31 +0900

    commit 3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBdhfIrUZqAhjQLl-qAuqcffAaqJ3TjyvM",
+  authDomain: "react-handson-test-kotomin.firebaseapp.com",
+  databaseURL: "https://react-handson-test-kotomin.firebaseio.com",
+  projectId: "react-handson-test-kotomin",
+  storageBucket: "",
+  messagingSenderId: "973140310841",
+  appId: "1:973140310841:web:e5d162f5036c46fa2f46e8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
